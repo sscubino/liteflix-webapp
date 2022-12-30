ba41d522079395bb45ee14bedaa98afa27130764
Santiago Cubino <sscubino@gmail.com>
2022-12-30 19:24:37 -0300

    change api base url to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   movies_api_base_url: 'https://api.themoviedb.org/3',
   movies_api_key: '6f26fd536dd6192ec8a57e94141f8b20',
   movies_api_configuration_cache_days_to_live: 7,
-  my_movies_list_api_base_url: 'http://scubino.pythonanywhere.com',
+  my_movies_list_api_base_url: 'https://scubino.pythonanywhere.com',
 };
